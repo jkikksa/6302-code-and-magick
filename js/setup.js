5bb234c30ec4e5fcc425835c9f1ece71ad78b5c6
Dmitry Rusakov <jkikksa@gmail.com>
2017-04-20 21:50:06 +0300

    corrections

--- js/setup.js
+++ js/setup.js
@@ -80,41 +80,5 @@
 
   window.makeDraggable(setupHandle, setup);
 
-  var shop = document.querySelector('.setup-artifacts-shop');
-  var inventory = document.querySelector('.setup-artifacts');
-  var draggedItem = null;
-
-  shop.addEventListener('dragstart', function (evt) {
-    if (evt.target.tagName.toLowerCase() === 'img') {
-      draggedItem = evt.target;
-      evt.dataTransfer.setData('text/plain', evt.target.alt);
-      inventory.classList.add('dropzone');
-    }
-  });
-
-  shop.addEventListener('dragend', function (evt) {
-    inventory.classList.remove('dropzone');
-  });
-
-  inventory.addEventListener('dragover', function (evt) {
-    evt.preventDefault();
-    return false;
-  });
-
-  inventory.addEventListener('drop', function (evt) {
-    evt.target.style.backgroundColor = '';
-    evt.target.appendChild(draggedItem);
-    inventory.classList.remove('dropzone');
-  });
-
-  inventory.addEventListener('dragenter', function (evt) {
-    evt.target.style.backgroundColor = 'yellow';
-    evt.preventDefault();
-  });
-
-  inventory.addEventListener('dragleave', function (evt) {
-    evt.target.style.backgroundColor = '';
-    evt.preventDefault();
-  });
 
 })();
